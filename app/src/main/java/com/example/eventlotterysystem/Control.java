69177b2e30ecdbe465fa70680fda7a2eaca29bd4
Longfei Jiao <ljiao@ualberta.ca>
2024-10-19 17:11:12 -0600

    Created Control.java

---
+++ app/src/main/java/com/example/eventlotterysystem/Control.java
@@ -0,0 +1,53 @@
+package com.example.eventlotterysystem;
+
+import java.util.ArrayList;
+
+public class Control {
+    // Attributes
+    private int currentUserID;
+    private int currentEventID;
+    private ArrayList<User> userList;
+    private ArrayList<Facility> facilityList;
+    private ArrayList<Event> eventList;
+    private ArrayList<Picture> pictureList;
+
+    // Constructor
+    public Control() {
+        this.currentUserID = 0;
+        this.currentEventID = 0;
+        this.userList = new ArrayList<>();
+        this.facilityList = new ArrayList<>();
+        this.eventList = new ArrayList<>();
+        this.pictureList = new ArrayList<>();
+    }
+
+    //getters
+
+    public int getCurrentEventID() {
+        int id = currentEventID;
+        currentEventID++;
+        return id;
+    }
+
+    public int getCurrentUserID() {
+        int id = currentUserID;
+        currentUserID++;
+        return id;
+    }
+
+    public ArrayList<User> getUserList() {
+        return userList;
+    }
+
+    public ArrayList<Facility> getFacilityList() {
+        return facilityList;
+    }
+
+    public ArrayList<Event> getEventList() {
+        return eventList;
+    }
+
+    public ArrayList<Picture> getPictureList() {
+        return pictureList;
+    }
+}
