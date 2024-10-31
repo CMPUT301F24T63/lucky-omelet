78b2c619842c646e3db5bdcf01e67364dcc486d9
“Zhen <zjiang10@ualberta.ca>
2024-10-31 13:49:21 -0600

    Return

--- app/src/main/java/com/example/eventlotterysystem/FacilityListActivity.java
+++ app/src/main/java/com/example/eventlotterysystem/FacilityListActivity.java
@@ -1,6 +1,9 @@
 package com.example.eventlotterysystem;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FacilityListActivity extends AppCompatActivity {
@@ -8,6 +11,13 @@ public class FacilityListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.facility_list_screen);
+        //  Return to the landing page
+        ImageButton returnButton = findViewById(R.id.return_button);
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
-
