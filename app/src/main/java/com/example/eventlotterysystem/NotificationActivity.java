97c5474057198ddf4bb2f3f3f639369a72b61316
Newvber <weibo2@ualberta.ca>
2024-11-07 22:56:12 -0700

    accept/decline well done, delete to be done

--- app/src/main/java/com/example/eventlotterysystem/NotificationActivity.java
+++ app/src/main/java/com/example/eventlotterysystem/NotificationActivity.java
@@ -24,19 +24,18 @@ public class NotificationActivity extends AppCompatActivity {
         setContentView(R.layout.notifications_screen);
 
         control = Control.getInstance();
-        control.setCurrentUser(control.getUserList().get(0));
+//        control.setCurrentUser(control.getUserList().get(0)); //just for testing
         curUser = control.getCurrentUser();
 
         ImageButton returnButton = findViewById(R.id.return_button);
         returnButton.setOnClickListener(view -> finish());
-//
+
         List = findViewById(R.id.notificationList);
 
-
+        // Populate notifications
         for (Notification noti : curUser.getNotificationList()) {
             addNotiToSection(noti, List);
         }
-
     }
 
     private void addNotiToSection(Notification noti, LinearLayout section) {
@@ -52,17 +51,35 @@ public class NotificationActivity extends AppCompatActivity {
         Button DeclineButton = NotifiView.findViewById(R.id.btnDecline);
         Button DeleteButton = NotifiView.findViewById(R.id.btnRemove);
 
-
-        if (noti.needAccept() == false) {
-            AcceptButton.setVisibility(View.INVISIBLE);
-            DeclineButton.setVisibility(View.INVISIBLE);
+        // if no need to accept/decline, disable buttons
+        if (!noti.needAccept()) {
+            AcceptButton.setEnabled(false);
+            DeclineButton.setEnabled(false);
         }
 
+        // handle accept button
         AcceptButton.setOnClickListener(v -> {
+            AcceptButton.setEnabled(false);
+            DeclineButton.setEnabled(false);
+            curUser.acceptInvitation(noti);
+            FirestoreManager.getInstance().saveControl(control);
         });
 
+        // handle decline button
         DeclineButton.setOnClickListener(v -> {
+            AcceptButton.setEnabled(false);
+            DeclineButton.setEnabled(false);
+            curUser.declineInvitation(noti);
+            FirestoreManager.getInstance().saveControl(control);
         });
+
+        // handle delete button
+        DeleteButton.setOnClickListener(v -> {
+            section.removeView(NotifiView);
+            curUser.removeNotification(noti);
+            FirestoreManager.getInstance().saveControl(control);
+        });
+
         section.addView(NotifiView);
     }
 }
